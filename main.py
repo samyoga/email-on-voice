ada21d4302a871a28463f21ec37964e25b0bea2d
Sunim Acharya <sunim.54@gmail.com>
2018-03-03 22:12:48 +0545

    Add imports

---
+++ main.py
@@ -0,0 +1,7 @@
+import os #system commands
+import smtplib #smtp protocol
+import time #time parameters
+
+import pyaudio #portaudio bindings
+import pyttsx3 #OFFLINE Python Text to Speech library
+import speech_recognition as sr #Library for speech recognition
