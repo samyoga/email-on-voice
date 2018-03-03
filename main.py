cbf518e0328671fceb653dbf9d5cdfa42282df7f
Sunim Acharya <sunim.54@gmail.com>
2018-03-03 22:17:55 +0545

    Microphone fetch email id

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ Initialize sr ans speech-engine
 '''
 r = sr.Recognizer()
 engine = pyttsx3.init()
+with sr.Microphone(device_index= None) as source: #Start microphone for email input
+        r.adjust_for_ambient_noise(source) #noise cancellation
+        engine.say("Email id") # Speaker -> Enter email id?
+        engine.runAndWait() #Loop for email id enter
+        time.sleep(5) # time interval for value like delay in microprocessor
+        audio = r.listen(source) # Listen to microphone source input
+        time.sleep(3) # timer
+        engine.say("Got it!") #Fetched the id
+        engine.runAndWait() #loop
